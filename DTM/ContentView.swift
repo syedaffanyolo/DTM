7158fb035c271702c49b08e3165e25c1e835da33
Syed Affan <Syedaffan070@gmail.com>
2020-08-24 08:08:13 +0530

    Initial Commit

---
+++ DTM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DTM
+//
+//  Created by Syedaffan on 8/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DTM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DTM
+//
+//  Created by Syedaffan on 8/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
